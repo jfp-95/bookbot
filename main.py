473eb19ec9d12220d15e68495a6c983c46b3de98
jpal95 <jackpalladino@gmail.com>
2025-04-15 09:22:30 -0400

    Adding bookbot code to Github

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from stats import accept_text
-from stats import num_of_chars
 
+import sys
+
+# Import functions from stats.py
+from stats import accept_text, num_of_chars, sorted_list
+
+#get book from filepath
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -8,11 +12,27 @@ def get_book_text(filepath):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    text = get_book_text(path)
+    print("=========BOOKBOT=========")
+    print("---------Word Count------------")
     accept_text(text)
-    num_of_chars(text)
+    print("---------Character Count-------------")
+    char_counts = num_of_chars(text)
+    sorted_chars = sorted_list(char_counts)
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+
+        if char.isalpha():
+            print(f"{char}: {count}")
 
 
+    print("=============END=============")
 
+if __name__ == "__main__":
+    main()
 
-main()
--- stats.py
+++ stats.py
@@ -4,16 +4,29 @@ def accept_text(words):
     words_ls = words.split()
     word_count = len(words_ls)
 
-    print(f'{word_count} words found in the document')
+    print(f'Found {word_count} total words')
 
 def num_of_chars(words):
     words = words.lower()
     char_counts = {}
-    for char in text:
+    for char in words:
         if char in char_counts: 
             char_counts[char] += 1
         else:
             char_counts[char] = 1
 
-    print(char_counts)
+    return char_counts
+
+def sorted_list(char_dict):
+    chars_list = []
+    for char, count in char_dict.items():
+        char_info = {
+            "char": char,
+            "count": count
+        }
+        chars_list.append(char_info)
+
+    chars_list.sort(reverse=True, key=lambda d: d["count"])
+
+    return chars_list
 
